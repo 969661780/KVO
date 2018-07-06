5cd8344bd12681c6c7939f841e9b67e8786b061c
969661780@qq.com <969661780@qq.com>
2018-07-06 13:30:46 +0800

    Initial Commit

---
+++ KVO/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KVO
+//
+//  Created by mt y on 2018/7/6.
+//  Copyright © 2018年 mt y. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KVO/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KVO
+//
+//  Created by mt y on 2018/7/6.
+//  Copyright © 2018年 mt y. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
